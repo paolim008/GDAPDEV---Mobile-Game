35abfb5117ae3a4f72352c0de57cada55db0adcf
paolim008 <pao_lim@dlsu.edu.ph>
2022-11-18 16:12:18 +0800

    LevelSelect
    Update

--- Assets/Scripts/LevelSelect/SwipeMenu.cs
+++ Assets/Scripts/LevelSelect/SwipeMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
@@ -39,5 +40,22 @@ public class SwipeMenu : MonoBehaviour
                 }
             }
         }
+
+        for (int i = 0; i < pos.Length; i++)
+        {
+            if (scrollPos < pos[i] + (distance / 2) && scrollPos > pos[i] - (distance / 2))
+            {
+                transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1f, 1f), 0.1f);
+                for (int j = 0; j < pos.Length; j++)
+                {
+                    if (j != i)
+                    {
+                        transform.GetChild(j).localScale = Vector2.Lerp(transform.GetChild(j).localScale,new Vector2(0.8f, 0.8f), 0.1f);
+                    }
+                }
+
+            }
+        }
+
     }
 }
