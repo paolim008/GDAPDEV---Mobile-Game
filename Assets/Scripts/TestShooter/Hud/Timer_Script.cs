71d5a9dc496b7358b25d7ad70e1cce118918f60a
paolim008 <pao_lim@dlsu.edu.ph>
2022-12-08 14:52:18 +0800

    AttackTimer Change

--- Assets/Scripts/TestShooter/Hud/Timer_Script.cs
+++ Assets/Scripts/TestShooter/Hud/Timer_Script.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using Time = UnityEngine.Time;
