e7ed16b9ab71c84f33ec4127f1803863ec6eb1e2
paolim008 <pao_lim@dlsu.edu.ph>
2022-12-08 16:30:45 +0800

    EnemyParticles

--- Assets/Scripts/TestShooter/Enemy/EnemyAi.cs
+++ Assets/Scripts/TestShooter/Enemy/EnemyAi.cs
@@ -12,6 +12,7 @@ public class EnemyAi : MonoBehaviour
     [SerializeField] private float damage = 10;
     [SerializeField] private GameObject spawnParticles;
     [SerializeField] private GameObject LExplosion;
+    [SerializeField] private GameObject AExplosion;
     private Animator anim;
 
     private ScoreManager scoreManager;
@@ -25,6 +26,7 @@ public class EnemyAi : MonoBehaviour
     void OnEnable()
     {
         Instantiate(spawnParticles, this.transform);
+        transform.LookAt(player.transform.position);
     }
     // Start is called before the first frame update
     void Awake()
@@ -62,7 +64,8 @@ public class EnemyAi : MonoBehaviour
     {
         anim.SetBool("Open_Anim", false);
         yield return new WaitForSeconds(1f);
-        Instantiate(LExplosion, this.transform.position, this.transform.rotation);
+        GameObject deathParticle = Instantiate(LExplosion, this.transform.position, this.transform.rotation);
+        Destroy(deathParticle, 2);
         Destroy(gameObject);
         scoreManager.AddScore(1);
     }
@@ -74,6 +77,8 @@ public class EnemyAi : MonoBehaviour
         attackCooldown = Random.Range(5, 10);
         yield return new WaitForSeconds(attackCooldown);
         anim.SetBool("Walk_Anim", true);
+        GameObject attackParticle = Instantiate(AExplosion, this.transform.position, this.transform.rotation);
+        Destroy(attackParticle, 2);
         yield return new WaitForSeconds(0.5f);
         anim.SetBool("Walk_Anim", false);
         player.GetComponent<Health>().TakeDamage(damage);
