d0624d1499e250496f905fef6d3c2afae725f1e5
paolim008 <pao_lim@dlsu.edu.ph>
2022-12-09 19:46:07 +0800

    MenuButton Implementation Final

--- Assets/Scripts/TestShooter/SceneLoader.cs
+++ Assets/Scripts/TestShooter/SceneLoader.cs
@@ -23,4 +23,9 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    private void ResetScore()
+    {
+        ScoreManager.instance.
+    }
+
 }
