07a14cb0ad23137807cdee50fd066b1cd9e412c5
Rizumuwu <miguel_paglinawan@dlsu.edu.ph>
2022-12-10 03:10:56 +0800

    b

--- Assets/Scripts/Initialize.cs
+++ Assets/Scripts/Initialize.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Initialize : MonoBehaviour
 {
--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -1,35 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerData : MonoBehaviour
 {
+    public string Name = "Rickshaw";
+    public int BaseHealth = 10, MaxHealth = 10, BaseShield = 3, MaxShield = 3, WeaponType = 0, Score = 0, BaseReloadTime = 1, ReloadMultiplier = 0, Coins = 0;
 
-    class PlayerInfo
-    {
-        string name;
-        int BaseHealth, MaxHealth, BaseShield, MaxShield, WeaponType, Score, BaseReloadTime, ReloadMultiplier, Coins;
-        
-        class Upgrades
-        {
-            int UpgradeHealth, UpgradeReloadTime, UpgradeShields;
-        }
+    public int UpgradeHealth = 0, UpgradeReloadTime = 0, UpgradeShield = 0;
 
-        class Settings
-        {
-            bool MusicMute, SfxMute;
-            float MusicVolume, SfxVolume;
-        }
-    }
-    // Start is called before the first frame update
-    void Start()
+    public bool MusicMute = false, SfxMute = false;
+    float MusicVolume = 0.5562919f, SfxVolume = 0.5715631f;
+
+    private static PlayerData instance;
+    public static PlayerData Instance
     {
-        
+        get { return instance; }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
     }
+
 }
